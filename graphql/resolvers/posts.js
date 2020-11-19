52033436db89468865f1ff28427012bb9bf02913
kwambokaB <paula.paulatek@gmail.com>
2020-11-19 14:53:54 +0300

    server validation for empty comment

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -31,6 +31,11 @@ module.exports = {
     Mutation: {
         async createPost(_, {body}, context){
             const user = checkAuth(context);
+
+            if(args.body.trim() === '') {
+               throw new Error('post  body must not be empty')
+            }
+            
             const newPost = new Post({
                 body,
                 use: user.id,
